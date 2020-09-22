46f12dfbaf9c5639ac9fc96243df8e18c16a3764
Marco Bambini <marco@creolabs.com>
2020-09-22 07:55:20 +0200

    Update buildAll.sh

--- gcc/buildAll.sh
+++ gcc/buildAll.sh
@@ -3,17 +3,20 @@
 # Build 64bit version
 make clean
 make
+rm ../CubeSQLPlugin/Build\ Resources/Linux\ x86-64/CubeSQLPlugin.so
 mv ./CubeSQLPlugin.so ../CubeSQLPlugin/Build\ Resources/Linux\ x86-64/
 
 # Build 32bit version
 make clean
 make bit32
+rm ../CubeSQLPlugin/Build\ Resources/Linux\ X86/CubeSQLPlugin.so
 mv ./CubeSQLPlugin.so ../CubeSQLPlugin/Build\ Resources/Linux\ X86/
 
 # Build ARM version
-export PATH="/root/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin:${PATH}"
 make clean
+export PATH="/root/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin:${PATH}"
 make -f MakefileARM
+rm ../CubeSQLPlugin/Build\ Resources/Linux\ ARM/CubeSQLPlugin.so
 mv ./CubeSQLPlugin.so ../CubeSQLPlugin/Build\ Resources/Linux\ ARM/
 
 # Cleanup
